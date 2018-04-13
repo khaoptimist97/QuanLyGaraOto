25987cfbad105f9a6bf7c11fb18ee2e9b9dd7087
khaoptimist97 <32835017+khaoptimist97@users.noreply.github.com>
2018-04-13 10:52:38 +0700

    UpLoad Project

---
+++ 60 Realisation/Source Code/QuanLyGaraOto/QuanLyGaraOto/Models/PhieuTiepNhan.cs
@@ -0,0 +1,30 @@
+namespace QuanLyGaraOto.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("PhieuTiepNhan")]
+    public partial class PhieuTiepNhan
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public PhieuTiepNhan()
+        {
+            PhieuSuaChuas = new HashSet<PhieuSuaChua>();
+        }
+
+        [Key]
+        public int IDPhieuTN { get; set; }
+
+        public int? IDBienSo { get; set; }
+
+        public DateTime? NgayTiepNhan { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<PhieuSuaChua> PhieuSuaChuas { get; set; }
+
+        public virtual Xe Xe { get; set; }
+    }
+}
---
+++ 60 Realisation/Source Code/QuanLyGaraOto/QuanLyGaraOto/Models/TienCong.cs
@@ -0,0 +1,29 @@
+namespace QuanLyGaraOto.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("TienCong")]
+    public partial class TienCong
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public TienCong()
+        {
+            ChiTietPhieuSuas = new HashSet<ChiTietPhieuSua>();
+        }
+
+        [Key]
+        public int IDTienCong { get; set; }
+
+        [StringLength(50)]
+        public string LoaiTC { get; set; }
+
+        public int? Gia { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<ChiTietPhieuSua> ChiTietPhieuSuas { get; set; }
+    }
+}
